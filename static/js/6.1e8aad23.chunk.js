e3bb2b6135eaee7256a8b1ff05d9cc8221069011
arkadiibasovych <arkadiybasovich@gmai.com>
2021-04-24 19:20:58 +0300

    Updates

---
+++ static/js/6.1e8aad23.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-09-phonebook"]=this["webpackJsonpgoit-react-hw-09-phonebook"]||[]).push([[6],{177:function(o,t,e){"use strict";e.r(t);var n=e(2);t.default=function(){return Object(n.jsxs)("div",{children:[Object(n.jsx)("h1",{children:"Phonebook"}),Object(n.jsx)("img",{src:"https://invent.kde.org/uploads/-/system/project/avatar/120/phonebook.svg.png",alt:"Phonebook"})]})}}}]);
+//# sourceMappingURL=6.1e8aad23.chunk.js.map
